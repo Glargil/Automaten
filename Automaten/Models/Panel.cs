df733f68acf02f544f634e716406b943b6c4908d
mak025 <mak025@edu.zealand.dk>
2025-08-27 10:57:28 +0200

    Adds Panel model
    Introduces a Panel model to manage inserted coins, input slot, change, and profit.
    
    Adds functionality to calculate the total value of inserted coins.

--- Automaten/Models/Panel.cs
+++ Automaten/Models/Panel.cs
@@ -4,5 +4,22 @@
     public class Panel
     {
         public List<Coins.Coins> InsertedCoins { get; set; } = new List<Coins.Coins>();
+
+        public string InputRowSlot { get; set; }
+
+        public int Change { get; set; }
+        public int Profit { get; set; }
+
+        public int CalcTotal()
+        {
+            int total = 0;
+            foreach (var coin in InsertedCoins)
+            {
+                total += coin.Value;
+            }
+            return total;
+        }
+
+
     }
 }
