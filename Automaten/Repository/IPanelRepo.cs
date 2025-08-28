203c6deff9bd3ee40955da30e6e0729610646d72
mak025 <mak025@edu.zealand.dk>
2025-08-28 11:45:17 +0200

    Refactored Panel.cs logic into service and repo layers

---
+++ Automaten/Repository/IPanelRepo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automaten.Repository
+{
+    public interface IPanelRepo
+    {
+        void InsertCoin(int coin);
+        int CalcTotal();
+    }
+}
